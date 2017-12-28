867d3ff19e22da491ea8dad60a02af0e2a2a54cc
Daniel <danielserrano00@gmail.com>
2017-12-28 17:21:51 -0500

    deleted default files, started from scratch.

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
 import { Form, FormControl, Button } from 'react-bootstrap'
+import Note from './Note'
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies'
 
+const cookie_key = 'NOTES'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      text: '',
+      notes: []
+    }
+  }
+
+  submit() {
+    const { notes, text } = this.state
+
+    notes.push({ text })
+
+    this.setState({ notes })
+
+    bake_cookie(cookie_key, this.state.notes)
+
+  }
+
   render() {
     return (
       <div>
         <h2>Note to Self</h2>
-        <Form>
-          <FormControl/>
-          <Button>Submit</Button>
+        <Form inline>
+          <FormControl onChange={event => this.setState({ text: event.target.value })}/>
+          {' '}
+          <Button onClick={() => this.submit()}>Submit</Button>
         </Form>
+        {
+          this.state.notes.map((note, index) => {
+            return (
+              <Note key={index} note={note}/>
+            )
+          })
+        }
+
       </div>
     )
   }
